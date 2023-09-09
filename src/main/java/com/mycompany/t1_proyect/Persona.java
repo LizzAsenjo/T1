a869ac3317d5a85d7b7ee36a1ff56cfbdafaa899
LizzAsenjo <142689925+LizzAsenjo@users.noreply.github.com>
2023-09-09 13:56:15 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/t1_proyect/Persona.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.t1_proyect;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Liz
+ */
+public class Persona {
+
+    private String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String consola(){
+           return  this.nombre;
+                   
+        
+    }
+}
---
+++ src/main/java/com/mycompany/t1_proyect/T1_Proyect.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t1_proyect;
+
+/**
+ *
+ * @author Liz
+ */
+public class T1_Proyect {
+
+    public static void main(String[] args) {
+        
+    }
+}
